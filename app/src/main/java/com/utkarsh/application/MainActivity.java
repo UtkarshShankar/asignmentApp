2378b8d774dc945751ebde20f0132bcc54a44256
UtkarshShankar <utkarshshankar9@gmail.com>
2021-12-23 21:43:28 +0530

    Second ui done

--- app/src/main/java/com/utkarsh/application/MainActivity.java
+++ app/src/main/java/com/utkarsh/application/MainActivity.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,6 +15,7 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     RecyclerView recyclerView;
+    Button purchase,sell;
     public List<Model> modalList=new ArrayList<Model>();
     public Adapter adapter;
     @Override
@@ -23,5 +27,18 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(linearLayoutManager);
         adapter=new Adapter(modalList);
         recyclerView.setAdapter(adapter);
+
+        purchase=findViewById(R.id.btnPurchase);
+        sell=findViewById(R.id.btnsell);
+
+        sell.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
